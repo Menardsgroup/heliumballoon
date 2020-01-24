29a46d55546aee2eceef7ead6dc72091e68240a0
Chuheng001 <41927316+chuheng001@users.noreply.github.com>
2020-01-24 01:12:22 +0000

    Update camera.py

--- Pi_Camera/camera.py
+++ Pi_Camera/camera.py
@@ -8,7 +8,7 @@ camera.resolution = camera.MAX_RESOLUTION
 "camera.framerate = 15"
 i = 0
 while True:
-    sleep(5)
+    sleep(30)
     camera.capture('/home/pi/pi_image/Still_Image/test%s.jpg' % i)
     camera.capture(format='jpeg', bayer=True, output='/home/pi/pi_image/Raw_Image/test%s.jpg' % i)
     i = i + 1
